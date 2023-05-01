54cf3fe426f87459ae4d1f0ec9a7b44b45a09517
Dmytro Tkachuk <68943595+demosua@users.noreply.github.com>
2023-05-01 21:21:23 +0300

    app-hooked

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -21,18 +21,23 @@ const App = () => {
   }
 
   const handleLoadMore = () => {
-    this.setPage(prevState => (prevState.page + 1));
+    setPage(prevState => (prevState + 1));
   }
 
   useEffect(() => {
-    setStatus('pending');
-      api
+    if (query) {
+      setStatus('pending');
+      api        
         .getImages(query, page)
-        .then(result => setImages(result => [...images, ...result]))
+        .then(result => {
+          setImages(prevState => [...prevState, ...result])
+          setStatus('resolved')
+        })
         .catch(error => {
           setStatus('rejected')
           toast.error(error.message)
         })
+    }
   }, [query, page]);
   
   return (
@@ -45,64 +50,5 @@ const App = () => {
     </Container>
     )
 }
-
-
-// class App extends Component {
-//   state = {
-//     query: '',
-//     page: 1,
-//     images: [],
-//     error: null,
-//     status: 'idle',
-//   };
-
-//   componentDidUpdate(_, prevState) {
-//     if (prevState.query !== this.state.query || prevState.page !== this.state.page) {
-//       this.setState({ status: 'pending' });
-
-//         api
-//           .getImages(this.state.query, this.state.page)
-//           .then(result => this.setState(prevState => {
-//             return { images: [...prevState.images, ...result], status: 'resolved' }
-//           }))
-//           .catch(error => {
-//             this.setState({ error, status: 'rejected' })
-//             toast.error(error.message)
-//           })
-      
-//     }
-//     if (this.state.page > 1) { this.scrollSmoothly() };
-//   }
-
-//   scrollSmoothly = () => {
-//     const cardHeight = 320;
-//       window.scrollBy({
-//         top: cardHeight * 2,
-//         behavior: "smooth",
-//       })
-//   };
-
-//   handleSubmit = query => {
-//     this.setState({ query, page: 1, images: [] });
-//   }
-
-//   handleLoadMore = () => {
-//     this.setState(prevState => ({ page: prevState.page + 1 }));
-//   }
-
-//   render() {
-//     const { images, status } = this.state;
-
-//     return (
-//       <Container>
-//         <Searchbar onSubmit={this.handleSubmit} />
-//         {status === 'rejected' && <ToastContainer autoClose={1000} />}
-//         {images.length > 0 && <ImageGallery images={images} />}
-//         {status === 'pending' && <Loader />}
-//         {images.length > 0 && <Button onClick={this.handleLoadMore} />}
-//       </Container>
-//       )
-//   }
-// }
 
 export default App;
